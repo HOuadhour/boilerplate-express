737b6f0d8e181dea7f7b3e379683a3fb45ad8124
HOuadhour <HOuadhour@Yandex.com>
2021-02-20 14:25:38 +0100

    Mount the express.static() middleware to the path /public with app.use()

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ var app = express();
 
 const path = __dirname + "/views/index.html";
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.sendFile(path));
 
 module.exports = app;
