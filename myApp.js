ffcb5515c0d485c7605b69c91cf90054d48d4484
HOuadhour <HOuadhour@Yandex.com>
2021-02-21 14:11:19 +0100

    Add MESSAGE_STYLE to .env file.

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ const path = __dirname + "/views/index.html";
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(path));
-app.get("/json", (req, res) => res.json({ message: "Hello json" }));
+app.get("/json", (req, res) =>
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json",
+  })
+);
 
 module.exports = app;
