8437ad1a5d5430e1cbacffd9f324b96c63519a0e
HOuadhour <HOuadhour@Yandex.com>
2021-02-21 13:37:39 +0100

    Serve an object as a response, in JSON format, to GET requests to the /json route.

--- myApp.js
+++ myApp.js
@@ -6,5 +6,6 @@ const path = __dirname + "/views/index.html";
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(path));
+app.get("/json", (req, res) => res.json({ message: "Hello json" }));
 
 module.exports = app;
